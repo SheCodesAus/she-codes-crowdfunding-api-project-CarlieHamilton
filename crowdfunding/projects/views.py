2d8e2cfbdcafffdaf5176409a5eb1e16bf49f845
CarlieHamilton <carliemhamilton@gmail.com>
2022-07-01 21:25:45 +1000

    adds users and permissions

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .permissions import IsOwnerOrReadOnly
 
+# /pledges/
 class PledgeList(APIView):
+    # GET request
     def get(self, request):
         pledges = Pledge.objects.all()
         serializer = PledgeSerializer(pledges, many=True)
         return Response(serializer.data)
 
+    # POST request
     def post(self, request):
         serializer = PledgeSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,16 +30,20 @@ class PledgeList(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+# /projects
 class ProjectList(APIView):
+
+    # GET request
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
 
+    # POST request
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
@@ -45,14 +53,33 @@ class ProjectList(APIView):
             status=status.HTTP_400_BAD_REQUEST
             )
 
-
+# /projects/<pk> - single project
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
         except Project.DoesNotExist: raise Http404
 
+    # GET request
     def get(self, request, pk):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    # PUT request (update)
+    def put(self, request, pk):
+      	project = self.get_object(pk)
+      	data = request.data
+      	serializer = ProjectDetailSerializer(
+           	instance=project,
+           	data=data,
+             	partial=True
+      	)
+      	if serializer.is_valid():
+           	serializer.save()
